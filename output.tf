f856f9d99ba3b4009452b2d313a6b59c5dce10bb
AsishSarangi <asishsarangi2005@gmail.com>
2025-07-18 13:08:44 +0530

    changes made

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "codebuild_project_name" {
   description = "CodeBuild project name"
-  value       = module.codebuild.project_name
+  value       = var.project_name
 }
 
 output "codepipeline_name" {
@@ -11,10 +11,6 @@ output "codepipeline_name" {
 output "s3_bucket_name" {
   description = "S3 bucket used in pipeline"
   value       = module.s3.bucket_name
-}
-
-output "ec2_public_ip" {
-  value = module.ec2.public_ip
 }
 
 output "codepipeline_role_arn" {
--- variable.tf
+++ variable.tf
@@ -1,1 +1,7 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "vite-react-app"
+}
+
 # nothing is here
