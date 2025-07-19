5e34a6b3df64440ea6f3571044c2ac69028bea59
AsishSarangi <asishsarangi2005@gmail.com>
2025-07-19 11:15:01 +0530

    automate

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "artifact_bucket" {
   bucket = var.bucket_name
+  force_destroy = true    # Automatically Empty the bucket and destroy it
 
   tags = {
     Name        = "${var.project_name}-artifact-bucket"
