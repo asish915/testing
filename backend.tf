acbab71ab57c261b4d60ebfcaefeca2569de02b8
AsishSarangi <asishsarangi2005@gmail.com>
2025-07-19 11:49:22 +0530

    automate

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-  terraform {
+terraform {
   backend "s3" {
-    bucket         = "test-ci-cd-sit-bucket"       # ✅ Replace with your actual bucket name
-    key            = "state/terraform.tfstate"
-    region         = "ap-south-1"                        # ✅ Use your AWS region (e.g., ap-south-1 for Mumbai)
-    encrypt        = true
+    bucket  = "test-ci-cd-sit-bucket" # ✅ Replace with your actual bucket name
+    key     = "state/terraform.tfstate"
+    region  = "ap-south-1" # ✅ Use your AWS region (e.g., ap-south-1 for Mumbai)
+    encrypt = true
   }
 }
--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.3"       # using stable version
+      version = "~> 6.3" # using stable version
     }
   }
 }
