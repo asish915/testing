0efc1e33b73b89d9a5e6f166145bb4e9ad2c7665
AsishSarangi <asishsarangi2005@gmail.com>
2025-07-19 13:25:24 +0530

    automate

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "artifact_bucket" {
-  bucket         = var.bucket_name
-  force_destroy  = true                   # Automatically empty the bucket
+  bucket        = var.bucket_name
+  force_destroy = true
 
   tags = {
     Name        = "${var.project_name}-artifact-bucket"
@@ -24,14 +24,30 @@ resource "aws_s3_bucket_public_access_block" "artifact_bucket_block" {
   restrict_public_buckets = true
 }
 
+resource "aws_kms_key" "s3_cmk" {
+  description = "CMK for S3 encryption"
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "artifact_bucket_encryption" {
   bucket = aws_s3_bucket.artifact_bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3_cmk.arn
     }
   }
 }
+
+resource "aws_s3_bucket" "artifact_bucket_logs" {
+  bucket = "${var.project_name}-artifact-bucket-logs"
+}
+
+resource "aws_s3_bucket_logging" "artifact_bucket_logging" {
+  bucket        = aws_s3_bucket.artifact_bucket.id
+  target_bucket = aws_s3_bucket.artifact_bucket_logs.id
+  target_prefix = "log/"
+}
+
 
 
